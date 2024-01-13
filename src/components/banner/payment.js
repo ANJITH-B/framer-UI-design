8d327f73b4b930766d15650a3a498fe1b0377465
ANJITH B <anjithb200@gmail.com>
2024-01-13 23:45:42 +0530

    added credit card image and toggle related

--- src/components/banner/payment.js
+++ src/components/banner/payment.js
@@ -1,10 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import ThemeBtn from '../setting/settings'
 
 const Payment = () => {
+    const backgroundImageUrl = useColorModeValue(
+        'https://pngimg.com/uploads/credit_card/credit_card_PNG204.png',
+        'https://www.bankbazaar.com/images/india/cc-image/large/common-credit-card.png'
+      );
+  
+
     return (
-        <><Box w={'18vw'} boxShadow='2xl' rounded='xl' shadow={'lg'} h={'5vw'} bg={'gray.500'}>
+        <><Box w={'18vw'} boxShadow='2xl' h={'14vw'} bg={`url('${backgroundImageUrl}') no-repeat center center`} backgroundSize="cover" rounded='xl'>
             <ThemeBtn/></Box></>
     )
 }
--- src/components/banner/themeToggle.js
+++ src/components/banner/themeToggle.js
@@ -2,6 +2,7 @@ import { Box } from '@chakra-ui/react'
 import React from 'react'
 import ThemeBtn from '../setting/settings'
 
+
 const ThemeToggle = () => {
     return (
         <><Box w={'18vw'} boxShadow='2xl' rounded='xl' shadow={'lg'} h={'5vw'} bg={'gray.500'}>
